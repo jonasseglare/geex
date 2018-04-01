e4422224f940f1fae889ec7751c8259aac3e5f82
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-04-01 22:29:30 +0200

    good

---
+++ src/lime/platform/core.clj
@@ -0,0 +1,43 @@
+(ns lime.codegen.core
+  (:require [bluebell.utils.defmultiple :refer [defmultiple defmultiple-extra]]
+            [bluebell.utils.core :as utils]
+            [insn.core :as insn]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Common utilities
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def known-platforms (atom #{:clojure
+                             :jvm}))
+
+(defn known-platform? [x]
+  (contains? (deref known-platforms) x))
+
+(defn register-platform
+  "When extending this library to support new paltforms (e.g. OpenCL), register it here"
+  [new-platform]
+  (swap! known-platforms conj new-platform))
+
+(defn dispatch-function
+  "A function that will dispatch on the platform key of the first argument"
+  [& args]
+  (let [platform (first args)]
+    (utils/data-assert (known-platform? platform)
+                       "Unknown platform" {:platform platform
+                                           :in args})
+    platform))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Code generators
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmultiple compile-static-value dispatch-function
+  (:clojure [platform value]
+            value))
+
