71a71be23954a024e342212a5ed15f72b0011ca4
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-04-26 17:46:58 +0200

    good

--- src/lime/platform/core.clj
+++ src/lime/platform/core.clj
@@ -1,6 +1,6 @@
 (ns lime.platform.core
 
-  "Everything that has to be platform specific"
+  "Platform specific code needed by the compiler"
   
   (:require [bluebell.utils.defmultiple :refer [defmultiple defmultiple-extra]]
             [bluebell.utils.core :as utils]
@@ -24,30 +24,6 @@
   "When extending this library to support new paltforms (e.g. OpenCL), register it here"
   [new-platform]
   (swap! known-platforms conj new-platform))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;  Persistent datastructure access
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn make-cast-seed [type value]
-  value)
-
-(defmultiple cast-to-type-sub defs/platform-dispatch
-  (defs/clojure-platform [type value] value) ;; TODO: indirect with the right type
-  (defs/java-platform [type value] (make-cast-seed type value)))
-
-(defn cast-to-type [type value]
-  (cast-to-type-sub type value
-                                        ;(core/to-seed value)
-                    ))
-
-
-
-
-
 
 
 
