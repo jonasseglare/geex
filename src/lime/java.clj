d6fdc84be01e379f71b4c4356d17b9ce2729e2f6
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-04-26 17:02:59 +0200

    good up to now

--- src/lime/java.clj
+++ src/lime/java.clj
@@ -1,7 +1,22 @@
 (ns lime.java
 
   "Generation of Java backed code"
-  
-  )
+
+  (:require [lime.java.defs :as jdefs]
+            [lime.core :as lime]
+            [clojure.spec.alpha :as spec]
+            [bluebell.utils.specutils :as specutils])
+  (:import [org.codehaus.janino SimpleCompiler]))
+
+(defn janino-cook-and-load
+  "Dynamically compile and load Java code as a class"
+  [class-name source-code]
+  (let [sc (SimpleCompiler.)]
+    (.cook sc source-code)
+    (let [cl (.loadClass (.getClassLoader sc) class-name)]
+      (.newInstance cl))))
 
 
+(defmacro typed-defn [& args0]
+  (let [args (specutils/force-conform ::jdefs/defn-args args0)]
+    (str args)))
