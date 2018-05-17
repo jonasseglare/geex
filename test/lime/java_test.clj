2760b4a5450d67f190f665456ab707cfe7a3b951
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-05-17 14:13:10 +0200

    cooking test right now

--- test/lime/java_test.clj
+++ test/lime/java_test.clj
@@ -1,2 +1,11 @@
 (ns lime.java-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [lime.java :refer :all]))
+
+(def c (time (janino-cook-and-load
+              "Kattskit"
+              "public class Kattskit {public double sq(double x) {return x*x;}}")))
+
+
+(deftest cooked-c-test
+  (is (= 81.0 (.sq c 9))))
