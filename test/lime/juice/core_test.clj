3df23999f598eb9d3fd2c0680ff9b8631ee0c12b
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-06-07 10:30:01 +0200

    clean up a bit

--- test/lime/juice/core_test.clj
+++ test/lime/juice/core_test.clj
@@ -7,16 +7,12 @@
 (deftest dispatch-fn-test
   (is (= [:prefixed :kattskit] (dispatch-code [:kattskit 119])))
   (is (= [:suffixed :kattskit] (dispatch-code [119 :kattskit])))
-  (is (= [:seed Double] (dispatch-code (lime/to-seed 1.3))))
+  (is (= [:seed Double/TYPE] (dispatch-code (lime/to-seed 1.3))))
   (is (= [:typed-map :ad] (dispatch-code {:type :ad :value 119})))
   (is (= :number (dispatch-code 119))))
 
 (deftest dispatch-code-vector-test
   (is (= (dispatch-code-vector [3.0 4])
          [[:seed java.lang.Double/TYPE] [:seed java.lang.Long/TYPE]]))
-  (is (= (basic-add-dispatch [1 2 3.0])
-         :result))
-  (is (= (basic-add-dispatch [1 2 3])
-         :result))
   (is (= (j/+ 1 2)
          3)))
