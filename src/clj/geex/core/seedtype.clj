409919955abfd5c8e0c29cb4b0f72474342a8158
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-06-07 13:31:54 +0200

    good as

---
+++ src/clj/geex/core/seedtype.clj
@@ -0,0 +1,13 @@
+(ns geex.core.seedtype
+  (:require [geex.core.seed :as sd]
+            [geex.core.datatypes :as dt])
+  (:refer-clojure :exclude [boolean char void byte short int long float double]))
+
+(defmacro inject-seed-defs []
+  `(do
+     ~@(map (fn [info]
+              `(def ~(symbol (str (.getName (:unboxed-type info))))
+                 (sd/typed-seed ~(dt/unboxed-type-symbol (:unboxed-name info)))))
+            dt/primitive-type-list)))
+
+(inject-seed-defs)
