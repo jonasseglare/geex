288787d547f9c8635d97b3b8b8acf7da6e86fb14
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-06-07 11:28:12 +0200

    Renamed to Geex

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lime "0.1.0-SNAPSHOT"
-  :description "Polhem is a library for writing simple and composable code that generates high-performance low level code, suitable for applications in optimization, image processing, machine learning, computer vision, graphics, games, statistics and many other domains."
+(defproject geex "0.1.0-SNAPSHOT"
+  :description "Geex is a library for writing simple and composable code that generates high-performance low level code, suitable for applications in optimization, image processing, machine learning, computer vision, graphics, games, statistics and many other domains."
   
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
