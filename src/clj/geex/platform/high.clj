baaa17a08c2e6c6076459a690f2ff4932454ef68
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-08-25 12:27:10 +0200

    clean up

--- src/clj/geex/platform/high.clj
+++ src/clj/geex/platform/high.clj
@@ -9,30 +9,3 @@
             [geex.core.stringutils :as su :refer [wrap-in-parens]]
             [bluebell.utils.core :as utils]
             [bluebell.utils.defmultiple :refer [defmultiple]]))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;  Implementation
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defmultiple compile-cast defs/platform-dispatch
-  (defs/java-platform
-    [comp-state expr cb]
-    (cb (defs/compilation-result
-          comp-state
-          (wrap-in-parens
-           ["(" (.getName (sd/datatype expr)) ")"
-            (-> expr
-                defs/access-compiled-deps
-                :value)])))))
-
-(defn cast-seed [type value]
-  (geex/with-new-seed
-    "cast-seed"
-    (fn [seed]
-      (-> seed
-          (sd/add-deps {:value value})
-          (sd/compiler compile-cast)
-          (sd/datatype type)))))
