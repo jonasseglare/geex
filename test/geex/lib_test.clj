3e996c4f791444cb25f22402d312db91d9d2a0ed
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-08-30 15:54:47 +0200

    still gooooooood

--- test/geex/lib_test.clj
+++ test/geex/lib_test.clj
@@ -10,7 +10,7 @@
 
 (gdb/set-expr-map-inspector viz/plot-expr-map)
 
-#_(typed-defn inc-vector-values :debug [clojure.lang.IPersistentVector src]
+(typed-defn inc-vector-values :debug [clojure.lang.IPersistentVector src]
             (lib/transduce
              (lib/map (comp lib/inc (partial lib/unwrap Double/TYPE)))
              lib/conj
