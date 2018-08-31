057f07fe3b0c5153f5231c1c0b94f75629b07b26
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-08-31 16:40:15 +0200

    at least this works

--- src/clj/geex/java/class.clj
+++ src/clj/geex/java/class.clj
@@ -1,104 +1,97 @@
 (ns geex.java.class
   (:require [clojure.spec.alpha :as spec]
+            [bluebell.utils.dsl :as dsl]
             [bluebell.utils.specutils :as specutils]))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;  Spec
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(spec/def ::class-def-body (spec/or :fn fn?
-                                    :vec (spec/coll-of ::class-def-body)))
+(declare public)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;  Implementation
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn init-class-def [name]
-  {:name name
+(def empty-class-def
+  {:name nil
    :extends []
    :implements []
    :methods []
-   :vars []
-   :static? false
-   :visibility :public})
+   :variables []
+   :current-settings {:static? false
+                      :visibility :public}
+   })
 
-(defn apply-to-class-def [class-def f]
-  (f class-def))
+(defn try-assoc [dst k v]
+  (if (map? dst)
+    (assoc dst k v)
+    dst))
 
-(defn push-key-value [dst k body-fn]
-  (let [backup (get dst k)]
-    (assoc (body-fn dst) k backup)))
+(defn conjer [key data]
+  (fn [state]
+    (update state key conj
+            (try-assoc data :settings (:current-settings state)))))
 
-(defn with-key-value [dst k value body-fn]
-  (push-key-value
-   dst k
-   (fn [dst]
-     (body-fn (assoc dst k value)))))
+(defn method-sub [method-data]
+  (conjer :methods method-data))
 
-(defn with-visibility [visibility & args]
-  (fn [class-def]
-    (with-key-value class-def :visibility visibility
-      (fn [class-def]
-        (reduce apply-to-class-def class-def args)))))
+(defn var-sub [var-data]
+  (conjer :variables var-data))
 
-(defn apply-body-to-class-def [class-def body]
-  (if (coll? body)
-    (reduce apply-body-to-class-def class-def body)
-    (body class-def)))
+(defn intoer [key values]
+  (fn [state] (update state key into values)))
 
-(defn add-classes [key & classes]
-  {:pre [(every? class? classes)]}
-  (fn [class-def]
-    (update class-def key into classes)))
+(def visibilities #{:public :private :protected})
 
+(defn class-spec-sub [name body]
+  (fn [state]
+    (dsl/do-body
+     (update state :name (fn [old-name]
+                           {:pre [(nil? old-name)]}
+                           name))
+     body)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;  Interface
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def private (partial with-visibility :private))
-(def public (partial with-visibility :public))
-(def protected (partial with-visibility :protected))
 
-(def implements (partial add-classes :implements))
-(def extends (partial add-classes :extends))
+(defmacro class-spec [name & body]
+  `(class-spec-sub (quote ~name) ~body))
 
-(defn static [& args]
-  (fn [class-def]
-    (with-key-value class-def :static? true
-      (fn [class-def]
-        (reduce apply-to-class-def class-def args)))))
+(defmacro method [name arglist & body]
+  {:pre (symbol? name)}
+  `(method-sub {:name (quote ~name)}))
 
-(defmacro variable [var-type var-symbol]
-  {:pre [(symbol? var-symbol)]}
-  `(fn [class-def#]
-     (update class-def#
-             :vars
-             conj
-             {:symbol (quote ~var-symbol)
-              :type ~var-type
-              :visibility (:visibility class-def#)
-              :static? (:static? class-def#)})))
+(defmacro variable [type name]
+  {:pre [(symbol? name)]}
+  `(var-sub {:name (quote ~name)
+             :type type}))
 
-(defmacro method [method-name arg-list & body]
-  {:pre [(symbol? method-name)
-         (vector? arg-list) ; <-- todo
-         ]}
-  `(fn [class-def#]
-     (update class-def#
-             :methods
-             conj
-             {:name (quote ~method-name)
-              :static? (:static? class-def#)
-              :visibility (:visibility class-def#)
-              ;;; TODO!!!
-              })))
+(defn implements [& interfaces]
+    {:pre [(every? class? interfaces)]}
+    (intoer :implements interfaces))
 
-(defmacro defclass [class-name & args]
-  `(let [args# ~(vec args)]
-     (specutils/validate ::class-def-body args#)
-     (apply-body-to-class-def (init-class-def (quote ~class-name)) args#)))
+(defn extends [& classes]
+  {:pre [(every? class? classes)]}
+  (intoer :extends classes))
+
+(defn static [& body]
+  (fn [state]
+    (dsl/with-updated
+      state
+      (dsl/path :current-settings :static?)
+      (constantly true)
+      body)))
+
+(defn visibility [v & body]
+  {:pre [(contains? visibilities v)]}
+  (fn [state]
+    (dsl/with-updated
+      state
+      (dsl/path :current-settings :visibility)
+      (constantly v)
+      body)))
+
+(def private (partial visibility :private))
+(def public (partial visibility :public))
+(def protected (partial visibility :protected))
--- test/geex/java/class_test.clj
+++ test/geex/java/class_test.clj
@@ -1,28 +1,29 @@
 (ns geex.java.class-test
   (:require [geex.java.class :refer :all :as jc]
+            [bluebell.utils.dsl :as dsl]
             [clojure.spec.alpha :as spec]
             [clojure.test :refer :all]))
 
-(deftest class-def-test
-  (let [x ((variable Double/TYPE a) (init-class-def 'Kattskit))]
-    (is (= 1 (count (:vars x))))
-    (let [y (-> x :vars first)]
-      (is (= y {:symbol 'a, :type Double/TYPE, :static? false, :visibility :public}))))
-  (is (= {:symbol 'a, :type Double/TYPE, :static? false, :visibility :private}
-         (-> ((private (variable Double/TYPE a)) (init-class-def 'Katt))
-             :vars
-             first)))
-  (is (= {:symbol 'a, :type Double/TYPE, :static? true, :visibility :private}
-         (-> ((private (static (variable Double/TYPE a))) (init-class-def 'Katt))
-             :vars
-             first)))
-  (is (= (-> (defclass Kattskit (private (variable Double/TYPE a)))
-             :vars
-             first
-             :visibility)
-         :private))
-  (let [c (defclass Katt 
-            [[[(extends java.lang.Double)
-               (implements java.lang.Integer)]]])]
-    (is (= [java.lang.Double] (:extends c)))
-    (is (= [java.lang.Integer] (:implements c))))) 
+(deftest basic-test
+  (let [class-spec (dsl/group
+                    (extends java.lang.Double)
+                    (implements java.lang.Integer)
+                    (static (method mummi []))
+                    (private (variable Double/TYPE kattskit))
+                    (variable Double/TYPE mjao)
+                    (implements java.lang.String))
+        result (class-spec
+                empty-class-def)
+        methods (:methods result)
+        m (first methods)]
+    (is (= [java.lang.Integer java.lang.String]
+           (:implements result)))
+    (is (= [java.lang.Double]
+           (:extends result)))
+    (is (= 1 (count methods)))
+    (is (contains? m :settings))
+    (is (:static? (:settings m)))
+    (is (= :public (:visibility (:settings m))))
+    (let [v (first (:variables result))]
+      (is (= :private (-> v :settings :visibility))))))
+
