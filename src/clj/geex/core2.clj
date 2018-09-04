5b874c2ad64bba5f0c15700a4be44d829e886534
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-09-04 20:56:08 +0200

    wip

--- src/clj/geex/core2.clj
+++ src/clj/geex/core2.clj
@@ -3,6 +3,8 @@
             [geex.core.seed :as seed]
             [geex.core.defs :as defs]))
 
+(declare make-seed)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;  Specs
@@ -12,15 +14,20 @@
 (spec/def ::counter number?)
 (spec/def ::seed-id ::counter)
 (spec/def ::seed-map (spec/map-of ::seed-id ::defs/seed))
-(spec/def ::injection-deps (spec/map-of any? ::seed-id))
-
+(spec/def ::seed-ref any?)
+(spec/def ::injection-deps (spec/map-of ::seed-ref ::seed-id))
 (spec/def ::state (spec/keys :req-un [::platform
                                       ::counter
                                       ::seed-map
                                       ::injection-deps]))
+(spec/def ::seed-with-id (spec/and ::defs/seed
+                                   (spec/keys :req-un [::seed-id])))
+
+(spec/def ::state-and-output (spec/cat :state ::state
+                                       :output any?))
 
 (def state? (partial spec/valid? ::state))
-
+(def state-and-output? (partial spec/valid? ::state-and-output))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;  Implementation
@@ -39,23 +46,55 @@
    :created-seeds []
    :injection-deps {}})
 
-(def ^:dynamic state nil)
+(def ^:dynamic state-atom nil)
 
 (defn swap-state! [f & args]
   (let [fargs (fn [x] (apply f (into [x] args)))]
-    (swap! state (comp ensure-state fargs ensure-state))))
+    (swap! state-atom (comp ensure-state fargs ensure-state))))
 
 (defn get-state []
   {:post [(state? %)]}
-  (if (nil? state)
+  (if (nil? state-atom)
     (throw (ex-info "No state bound, are you calling 
 it outside of with-state?" {}))
-    (deref state)))
+    (deref state-atom)))
 
 (defn step-counter [state]
   {:pre [(state? state)]
    :post [(state? %)]}
   (update state :counter inc))
+
+(defn registered-seed? [x]
+  (and (seed/seed? x)
+       (contains? x :seed-id)))
+
+;; Should take anything
+(defn to-seed-in-state [state x]
+  {:pre [(state? state)]
+   :post [(state-and-output? %)]}
+  (cond
+    (registered-seed? x) [state x]
+    (seed/seed? x) (make-seed state x)))
+
+(defn import-deps [state seed-prototype]
+  (let [deps (vec (seed/access-deps-or-empty seed-prototype))
+        [state deps] (reduce
+                      (fn [[state mapped-deps] [k v]]
+                        (let [[state v] (to-seed-in-state )]))
+                      [state []]
+                      deps)]))
+
+(defn register-seed [state seed-prototype]
+  (let [[state seed-prototype] (import-deps state seed-prototype)]))
+
+(defn make-seed [state seed-prototype]
+  {:pre [(state? state)
+         (defs/seed? seed-prototype)
+         (not (registered-seed? seed-prototype))]
+   :post [(spec/valid? ::state-and-output %)]}
+  [(-> state
+        step-counter
+        (register-seed seed-prototype)) nil])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -73,6 +112,9 @@ it outside of with-state?" {}))
   {:pre [(state? init-state)
          (fn? body-fn)]
    :post [(state? %)]}
-  (binding [state (atom init-state)]
+  (binding [state-atom (atom init-state)]
     (body-fn)
-    (deref state)))
+    (deref state-atom)))
+
+;; Create a new seed state flushes the bindings
+(defn flush-bindings! [])
