e89f116e2b6f828a0c3f2db63ec5aa8bcc4830cf
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-10-11 16:59:33 +0200

    good so f

---
+++ src/java/geex/LocalBindings.java
@@ -0,0 +1,31 @@
+package geex;
+
+import java.util.ArrayList;
+
+public class LocalBindings {
+    private int _counter = 0;
+    ArrayList<Binding> _bindings = new ArrayList<Binding>();
+
+    public void clear() {
+        _bindings = new ArrayList<Binding>();
+    }
+
+    public ArrayList<Binding> bindings() {
+        return _bindings;
+    }
+
+    public Binding addBinding(Seed seed) {
+        if (!seed.hasCompilationResult()) {
+            throw new RuntimeException(
+                "Seed " + seed.toString() 
+                + " does not have a compilation result");
+        }
+        Object result = seed.getCompilationResult();
+        Binding b = new Binding(
+            seed.generateVarName(),
+            seed.getType(),
+            result);
+        _bindings.add(b);
+        return b;
+    }
+}
---
+++ src/java/geex/LocalVar.java
@@ -0,0 +1,10 @@
+package geex;
+
+public class LocalVar {
+
+    private int _index = -1;
+    
+    public LocalVar(int i) {
+        _index = i;
+    }
+}
