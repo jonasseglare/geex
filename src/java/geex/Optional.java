54508f3aae89ae99617f021a0dde73bce7833c8f
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-10-12 12:04:28 +0200

    bra så va

---
+++ src/java/geex/Optional.java
@@ -0,0 +1,38 @@
+package geex;
+
+
+// Can also have null as a value
+public class Optional<T> {
+    private boolean _hasValue = false;
+    public T _value = null;
+
+    public Optional(T x) {
+        _hasValue = true;
+        _value = x;
+    }
+
+    public Optional() {}
+
+    public boolean hasValue() {
+        return _hasValue;
+    }
+
+    public boolean isPresent() {
+        return _hasValue;
+    }
+    
+    public T get() {
+        if (!_hasValue) {
+            throw new RuntimeException("No value present");
+        }
+        return _value;
+    }
+
+    public static<T> Optional of(T value) {
+        return new Optional(value);
+    }
+
+    public static<T> Optional empty() {
+        return new Optional();
+    }
+}
