09eb705c9bd778c20a4ddf8f01055af5bc504f9f
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-10-12 14:39:44 +0200

    ganska bra vae

---
+++ src/java/geex/JavaPlatformFunctions.java
@@ -0,0 +1,10 @@
+package geex;
+
+import geex.PlatformFunctions;
+
+public class JavaPlatformFunctions 
+    implements PlatformFunctions {
+    public Object renderLocalVarName(String s) {
+        return s;
+    }
+}
