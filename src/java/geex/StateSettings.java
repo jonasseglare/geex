9ee19401fe544262a4e561cfec05c6958d522db8
Jonas Östlund <uppfinnarjonas@gmail.com>
2019-03-22 10:52:01 +0100

    good

--- src/java/geex/StateSettings.java
+++ src/java/geex/StateSettings.java
@@ -3,15 +3,11 @@ package geex;
 import clojure.lang.IFn;
 
 public class StateSettings {
-    public PlatformFunctions platformFunctions = null;
     public Object platform = null;
     public IFn forwardedFunction = null;
     public IFn closeScope = null;
 
     void check() {
-        if (platformFunctions == null) {
-            throw new RuntimeException("No platform functions");
-        }
         if (platform == null) {
             throw new RuntimeException("No platform specified");
         }
