ca8cc8c5b7d236d315d69fa6537c34ab5e1d1613
Jonas Östlund <uppfinnarjonas@gmail.com>
2019-03-22 11:03:59 +0100

    state test

---
+++ test/geex/State_test.clj
@@ -0,0 +1,16 @@
+(ns geex.State-test
+  (:import [geex
+            State
+            StateSettings
+            ClojurePlatformFunctions]
+           [clojure.lang PersistentHashMap])
+  (:require [clojure.test :refer :all]
+            [bluebell.utils.wip.java :as jutils :refer [set-field]]))
+
+(deftest construct-state-test
+  (let [state (State.
+               (doto (StateSettings.)
+                 (set-field platform :clojure)
+                 (set-field closeScope (fn [] (assert false)))))]
+    (is state)
+    (is (.isEmpty state))))
